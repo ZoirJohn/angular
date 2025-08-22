2bab810756796790f51da3d071d1af5f5f716a95
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-08-23 01:55:28 +0500

    Integrate HTTP client and fetch products from API
    - Added `provideHttpClient` to app configuration for HTTP requests.
    - Updated app component to fetch products using the Products service and display them in a grid layout.
    - Refactored app HTML to dynamically render product cards with images and descriptions.
    - Enhanced styling for product cards and headers.
    - Implemented a service to retrieve product data from a local API endpoint.
    - Configured environment variables to set the base URL for the API.
    - Added Tailwind CSS for improved styling capabilities.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { Products } from './services/products';
+import { IProduct } from './models/products';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatCardModule,MatButtonModule],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, NgOptimizedImage, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-
+  productsService = inject(Products);
+  products = signal<IProduct[]>([]);
+  constructor() {
+    this.productsService.getProducts().subscribe({
+      next: (data) => {
+        this.products.set(data);
+      },
+      error: (error) => console.error(error),
+    });
+  }
 }
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { IProduct } from '../models/products';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Products {
-  client = inject(HttpClient)
-  getProducts() {
-    return this.client.get<IProduct[]>(environment.baseUrl)
+  client = inject(HttpClient);
+  getProducts(): Observable<IProduct[]> {
+    return this.client.get<IProduct[]>(environment.baseUrl);
   }
 }
